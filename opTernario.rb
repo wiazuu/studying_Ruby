0896200de6a001805ee1771905850e9e64654fd2
wiazuu <lucaspron@proton.me>
2023-07-26 20:21:33 -0300

    +

--- opTernario.rb
+++ opTernario.rb
@@ -1,10 +1,11 @@
 #para usar operadores ternarios usamos -> Condiçao ? verdadeiro : falso ,exemplo abaixo
 nome = "Lucas"
 
-puts nome.eql?("Lucs") ? "dono do repositorio" : "Visita"
+
+print nome.eql?("Lucas") ? " dono do repositorio " : " Visita "
 # isso diminuirá esse codigo
 if nome == ("Lucas")
-    puts ("dono do repositorio")
+    puts (" dono do repositorio ")
 else
     puts "Visitante"
 end
