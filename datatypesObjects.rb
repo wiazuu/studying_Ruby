419fa269e17e342ff13d635067fc97f1c0c44867
wiazuu <lucaspron@proton.me>
2023-07-28 04:32:03 -0300

    Datatypes

---
+++ datatypesObjects.rb
@@ -0,0 +1,43 @@
+# Number
+num1 = 5        # Integer (Fixnum)
+num2 = 5.5      # Float
+
+puts "Números: #{num1}, #{num2}"
+
+# String
+str = "Hello, World!"
+puts "String: #{str}"
+
+# Array
+arr = [1, "two", 3.0]
+puts "Array: #{arr}"
+
+# Hash
+hash = { "name" => "Bob", "age" => 30 }
+puts "Hash: #{hash}"
+
+# Symbol
+sym = :symbol
+puts "Symbol: #{sym}"
+
+# Boolean
+t = true
+f = false
+puts "Booleanos: #{t}, #{f}"
+
+# Nil
+n = nil
+puts "Nil: #{n}"
+
+# Range
+range = 1..10
+puts "Range: #{range}"
+
+# Uso do range em um loop
+puts "Números no intervalo: "
+range.each do |n|
+  puts n
+end
+
+
+#esses sao exemplos de datatypes usadas de forma basica
